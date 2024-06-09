3f0fd0d12b6688fe4b3fa62bef3c94f9ceba5f0a
sdzmnk <sdzmnk@gmail.com>
2024-06-09 09:26:54 -0700

    commit

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,24 +1,24 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Забули пароль? Немає проблем. Просто повідомте нам свою електронну адресу, і ми надішлемо вам посилання для скидання пароля, яке дозволить вам вибрати новий.') }}
     </div>
 
-    <!-- Session Status -->
+    <!-- Статус сеансу -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <!-- Email Address -->
+        <!-- Електронна адреса -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Електронна адреса')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Надіслати посилання для скидання пароля') }}
             </x-primary-button>
         </div>
     </form>
